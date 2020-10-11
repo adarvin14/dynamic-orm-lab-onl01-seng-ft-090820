e3ad92ed667a38e07f905d3673377c8a0a82a76d
Alex Darvin <amdarvin14@gmail.com>
2020-10-11 19:59:37 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -5,13 +5,12 @@ class InteractiveRecord
   
   def self.table_name
     self.to_s.downcase.pluralize
-  end 
+  end
   
   def self.column_names
-     DB[:conn].results_as_hash = true
-
+    DB[:conn].results_as_hash = true
+    
     sql = "pragma table_info('#{table_name}')"
-
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |row|
@@ -20,7 +19,7 @@ class InteractiveRecord
     column_names.compact
   end
   
-  def initialize(options={})
+  def intitialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
     end
@@ -32,25 +31,25 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    DB[:conn].execute(sql, name)
+  end
+  
   def table_name_for_insert
     self.class.table_name
   end
-
+  
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+  
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
     values.join(", ")
-  end
-
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end
-  
-  def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-    DB[:conn].execute(sql, name)
   end
   
   def self.find_by(attribute) 
@@ -59,5 +58,5 @@ class InteractiveRecord
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_key} = '#{attribute_value}'LIMIT 1"
      DB[:conn].execute(sql)
   end
-  
+    
 end
