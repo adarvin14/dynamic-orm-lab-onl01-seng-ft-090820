fa500215490e84d418b27394aec4e3a3b018d654
Alex Darvin <amdarvin14@gmail.com>
2020-10-11 19:48:02 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -14,7 +14,7 @@ class InteractiveRecord
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |row|
-      column_names << 
+      column_names << row
   end
   
   def intitialize(options={})
